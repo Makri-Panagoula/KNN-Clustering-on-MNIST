ea9b62fcceff3e2d200af4c520f7bfd617e971e7
Makri-Panagoula <panagoulamakri@gmail.com>
2023-11-13 23:00:23 +0200

    Input Processing

--- Makefile
+++ Makefile
@@ -8,6 +8,8 @@ CC = g++
 # Compile Options
 CFLAGS = -Wall -Werror -I -g -I$(HEADERS)
 
+#----------------------LSH make, run & valgrind-----------------------------
+
 LSH = main_lsh
 OBJ_L = $(SRC)/lsh.o $(SRC)/img.o $(SRC)/hFunc.o $(SRC)/hashTable.o $(SRC)/input.o
 OBJ_LSH = main_lsh.o $(OBJ_L)
@@ -19,6 +21,12 @@ $(LSH): $(OBJ_LSH)
 
 run_lsh: $(LSH)
 	./$(LSH) $(ARGSL)
+
+valgrind_lsh:	$(LSH)
+	valgrind --track-origins=yes --leak-check=full ./$(LSH) $(ARGSL)
+
+
+#----------------------Hypercube make, run & valgrind----------------------
 
 CUBE = main_cube 
 OBJC = $(SRC)/img.o $(SRC)/hFunc.o $(SRC)/input.o $(SRC)/cube.o 
@@ -32,8 +40,13 @@ $(CUBE): $(OBJ_CUBE)
 run_cube: $(CUBE)
 	./$(CUBE) $(ARGSC)
 
+valgrind_cube:	$(CUBE)
+	valgrind --track-origins=yes --leak-check=full ./$(CUBE) $(ARGSC)
+
+
+#----------------------Cluster make, run & valgrind----------------------
+
 CLUSTER = main_cluster
-OBJC = $(SRC)/img.o $(SRC)/hFunc.o $(SRC)/input.o $(SRC)/cube.o 
 OBJ_CL = main_cluster.o $(OBJ_L) $(SRC)/cube.o $(SRC)/cluster.o
 
 ARGSCL = –i datasets/input.dat –c cluster.conf -o output_cluster -m Classic
@@ -44,14 +57,24 @@ $(CLUSTER): $(OBJ_CL)
 run_cluster: $(CLUSTER) $(LSH) $(CUBE)
 	./$(CLUSTER) $(ARGSCL)
 
-valgrind_lsh:	$(LSH)
-	valgrind --track-origins=yes --leak-check=full ./$(LSH) $(ARGSL)
-
-valgrind_cube:	$(CUBE)
-	valgrind --track-origins=yes --leak-check=full ./$(CUBE) $(ARGSC)
-
 valgrind_cluster:  $(CLUSTER)
 	valgrind --track-origins=yes --leak-check=full ./$(CLUSTER) $(ARGSCL)
 
+
+#----------------------Graph Search make, run & valgrind----------------------
+
+GRAPH_SEARCH = graph_search
+OBJS_G = graph_search.o $(OBJ_L) $(SRC)/cube.o
+ARGS_G = –d datasets/input.dat –q datasets/query.dat –k 7 -E 200 -R -N 5 -l 15 -m 1 -o output_graph
+
+$(GRAPH_SEARCH): $(OBJS_G)
+	$(CC) $(CFLAGS) $(OBJS_G) -o $(GRAPH_SEARCH) -lm -g3
+
+run_graph: $(GRAPH_SEARCH) $(LSH) $(CUBE)
+	./$(GRAPH_SEARCH) $(ARGS_G)
+
+valgrind_graph:  $(GRAPH_SEARCH)
+	valgrind --track-origins=yes --leak-check=full ./$(GRAPH_SEARCH) $(ARGS_G)
+
 clean:
-	rm -f $(OBJ_CL) $(OBJ_LSH) $(OBJ_CUBE) $(LSH) $(CUBE) $(CLUSTER) output*
+	rm -f $(OBJS_G) $(OBJ_CL) $(OBJ_LSH) $(OBJ_CUBE) $(LSH) $(CUBE) $(CLUSTER) $(GRAPH_SEARCH) output*
---
+++ graph_search.cpp
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string> 
+#include <cstring>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <sstream>
+#include "headers/lsh.h"
+#include "headers/cube.h"
+#include <map>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+
+    string input_file = argv[2] ;
+    string query_file ;
+
+    //Query file is an optional command line argument
+    if ( strcmp(argv[4],"–k") ) {
+        query_file = argv[4] ;
+        argv++;                     //Next character will be "-k" => skip reading it , move into next one 
+    }
+
+    //K-Nearest neighbours in Graph
+    int k = 50 ;                       
+    //If there is a value after "-k",update k with that
+    if ( strcmp(argv[5],"-E") ) {
+        k = atoi(argv[5]) ;
+        argv++;                     
+    }
+    
+    //Number of Extensions
+    int E = 30 ;                       //Default Value
+    if ( strcmp(argv[6],"-R") ) {
+        E = atoi(argv[6]) ;
+        argv++;                     
+    }
+
+    //Number of Randomized Reruns
+    int R = 1 ;                       //Default Value
+    if ( strcmp(argv[7],"-N") ) {
+        R = atoi(argv[7]) ;
+        argv++;                     
+    }
+
+    //Wanted Nearest Neighbours
+    int N = 1 ;                       //Default Value
+    if ( strcmp(argv[8],"-l")) {
+        N = atoi(argv[8]) ;
+        argv++;                     
+    }    
+
+    //Number of candidates (only used for MRNG)
+    int l = 20 ;                       //Default Value
+    if ( strcmp(argv[9],"-m")) {
+        l = atoi(argv[9]) ;
+        argv++;                     
+    }  
+
+    //Method to use (1 for GNNS, 2 for MRNG)
+    int m = atoi(argv[10]) ;                       //Default Value
+
+    //Output file is optional command line argument
+    string output_file ;
+    if (argv[12] != NULL) {
+        output_file = argv[12] ;
+    }  
+
+    //Read image input dataset
+    Input* imgs = new Input(input_file);
+
+    //Create Search Structure
+
+    int runs = 0 ;
+    string answer;
+    do {
+        if ( runs > 0 || query_file.empty())  {  //If user hasn't passed it as command line argument 
+            cout<<"Please give the path to query dataset file !"<<endl;
+            cin >> query_file;
+        }
+        if ( runs++ > 0 || output_file.empty())  {  //If user hasn't passed it as command line argument 
+            cout<<"Please give the path to output file !"<<endl;
+            cin >> output_file;
+        }    
+        ifstream query(query_file, ios::binary | ios::in);
+        if(! query.is_open()) {
+            cout << "Failed to read query dataset file!" << endl;
+            exit;
+        }        
+        //Read a small sample of images in the query dataset and perform the algorithms on them
+        for(int i = 0; i < 2; i++) {
+            Img* query_point = new Img(imgs->get_pxs(),i+1,query);
+            // lsh->queryNeighbours(query_point,N,output_file,R);
+            delete query_point;
+        }   
+        do {
+            cout<<"Would you like to continue execution for a different query dataset? Please enter y / N !"<<endl;
+            cin >> answer;
+        }while(answer != "y" && answer != "N");
+        
+    }while(answer == "y");
+
+    delete imgs;
+    return 0;
+}
